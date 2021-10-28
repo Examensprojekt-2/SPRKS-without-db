9a423b7b3b2c9809b833b5b426884ebbf8a93842
filmol <40041318+filmol@users.noreply.github.com>
2021-10-28 13:58:24 +0200

    add function folder with empty templates

---
+++ sprks/functions/functions.js
@@ -0,0 +1,13 @@
+export function getRecommendedGames() {
+  return null;
+}
+export function getPopularGames() {
+  return null;
+}
+export function getFriendsPlaying() {
+  return null;
+}
+export function toggleFriendsAside() {
+  // document.getElementById('friendsComponent').toggle('hidden');
+  console.log('toggle');
+}
