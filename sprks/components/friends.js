8042cddba55e5b7553856ed0b483dcb0b587bb3f
albin-ux <62427356+albin-ux@users.noreply.github.com>
2021-11-04 10:38:24 +0100

    Revert "Merge branch 'friends' into dev"
    This reverts commit 050562470ad6e39fc5d2cb2c5c08f523aab3c19d, reversing
    changes made to f336a9ee5f21255ceaa97211efaf5dcb2275d2a0.

--- sprks/components/friends.js
+++ sprks/components/friends.js
@@ -2,92 +2,56 @@ import { users } from '../database/user';
 
 function Friends() {
   return (
-    <div class="w-full max-w-screen-xl mx-auto mt-4 p-6">
-  <div class="relative rounded overflow-hidden border border-grey-light mb-8 mt-1 bg-white">
-      <div class="bg-white overflow-hidden">
-
-        <div class="w-4/5 h-10 py-3 px-1">
-        <img
-            class="h-16 w-16 rounded-full ml-7 mt-1"
-            src="https://randomuser.me/api/portraits/men/24.jpg"
-            alt="Randy Robertson"
-          />
-          <p class="hover:text-blue-dark text-center font-bold">{users[0].name}</p>
+    <div>
+      <div>
+        <div className='py-2 text-xl text-green-500'>
+          <h1>Online</h1>
         </div>
-        <div class="sm:flex sm:items-center px-2 py-4 mt-8">
-          <div class="flex-grow">
-            <h3 class="font-normal px-2 py-3">Friends</h3>
-            <input type="text" placeholder="Search teams or members"
-              class="my-2 w-full text-sm bg-grey-light text-grey-darkest rounded h-10 p-3 focus:outline-none mt-0" />
-            <div class="w-full">
-              <div class="flex cursor-pointer my-1 hover:bg-blue-lightest rounded">
-                <div class="w-8 h-10 text-center py-1">
-                  <p class="text-3xl p-0 text-green-dark">&bull;</p>
-                </div>
-                <div class="w-4/5 h-10 py-3 px-1">
-                  <p class="hover:text-blue-dark">{users[0].friends[0].name}</p>
-                </div>
-                <div class="w-1/5 h-10 text-right p-3">
-                  <p class="text-sm text-grey-dark">Member</p>
-                </div>
-              </div>
-              <div class="flex cursor-pointer my-1 hover:bg-blue-lightest rounded">
-                <div class="w-8 h-10 text-center py-1">
-                  <p class="text-3xl p-0 text-grey-dark">&bull;</p>
-                </div>
-                <div class="w-4/5 h-10 py-3 px-1">
-                  <p class="hover:text-blue-dark">{users[0].friends[1].name}</p>
-                </div>
-                <div class="w-1/5 h-10 text-right p-3">
-                  <p class="text-sm text-grey-dark">Team</p>
-                </div>
-              </div>
-              <div class="flex cursor-pointer my-1 hover:bg-blue-lightest rounded">
-                <div class="w-8 h-10 text-center py-1">
-                  <p class="text-3xl p-0 text-green-dark">&bull;</p>
-                </div>
-                <div class="w-4/5 h-10 py-3 px-1">
-                  <p class="hover:text-blue-dark">{users[0].friends[2].name}</p>
-                </div>
-                <div class="w-1/5 h-10 text-right p-3">
-                  <p class="text-sm text-grey-dark">Member</p>
-                </div>
-              </div>
-              <div class="flex cursor-pointer my-1 hover:bg-blue-lightest rounded">
-                <div class="w-8 h-10 text-center py-1">
-                  <p class="text-3xl p-0 text-grey-dark">&bull;</p>
-                </div>
-                <div class="w-4/5 h-10 py-3 px-1">
-                  <p class="hover:text-blue-dark">{users[0].friends[3].name}</p>
-                </div>
-                <div class="w-1/5 h-10 text-right p-3">
-                  <p class="text-sm text-grey-dark">Team</p>
-                </div>
-              </div>
-              <div class="flex my-1 cursor-pointer hover:bg-blue-lightest rounded">
-                <div class="w-8 h-10 text-center py-1">
-                  <p class="text-3xl p-0 text-grey-dark">&bull;</p>
-                </div>
-                <div class="w-4/5  h-10 py-3 px-1">
-                  <p class="hover:text-blue-dark">{users[0].friends[4].name}</p>
-                </div>
-                <div class="w-1/5 h-10 text-right p-3">
-                  <p class="text-sm text-grey-dark">Member</p>
-                </div>
-              </div>
-            </div>
+        <div className='text-lg text-white border-4 border-opacity-50 rounded-lg border-light-blue-500'>
+          <div className='px-2 py-2'>
+            <p className='py-0.5'>
+              Jessen{' '}
+              <p className='inline text-green-500 text-opacity-75'>
+                is playing{' '}
+              </p>
+              <p className='inline'>Fortnite</p>
+            </p>
+            <p className='py-0.5'>
+              Adam{' '}
+              <p className='inline text-blue-500 text-opacity-75'>is online</p>
+            </p>
+            <p className='py-0.5'>
+              Erik{' '}
+              <p className='inline text-blue-500 text-opacity-75'>is online</p>
+            </p>
+            <p className='py-0.5'>
+              Oscar{' '}
+              <p className='inline text-blue-500 text-opacity-75'>is online</p>
+            </p>
           </div>
         </div>
-        <div class="sm:flex bg-grey-light sm:items-center px-2 py-4 flex justify-center color  ">
-          <div class="">
-            <button class="text-white hover:text-grey-dark py-2 px-4 rounded bg-blue-700">
-              Add friend?
-            </button>
+        <div className='py-2 text-xl text-gray-500'>
+          <h1>Offline</h1>
+        </div>
+        <div className='text-lg text-white border-4 border-opacity-50 rounded-lg border-light-blue-500'>
+          <div className='px-2 py-2'>
+            <p className='py-0.5'>
+              Filip{' '}
+              <p className='inline text-gray-100 text-opacity-50'>
+                4 day(s) ago
+              </p>
+            </p>
+            <p className='py-0.5'>
+              Marcus{' '}
+              <p className='inline text-gray-100 text-opacity-50'>
+                1 day(s) ago
+              </p>
+            </p>
           </div>
         </div>
       </div>
-  </div>
-</div>
-  )
+    </div>
+  );
 }
+
 export default Friends;
