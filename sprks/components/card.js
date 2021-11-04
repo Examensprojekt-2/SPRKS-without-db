d07c11bb02667bf990bc5caf4b2f5da9e87b3303
wiktorsson <wiktorsson@live.se>
2021-11-04 15:27:41 +0100

    Changed button color

--- sprks/components/card.js
+++ sprks/components/card.js
@@ -7,11 +7,11 @@ function Card({ picture, text, likes, title, tags }) {
         <p class='text-gray-700 text-base'>{text}</p>
       </div>
       <div class='text-center'>
-      <button class='bg-yellow-200 hover:bg-yellow-300 text-white font-bold py-2 px-4 rounded-full'>
-          <svg xmlns='http://www.w3.org/2000/svg' className='h-6 w-6' fill='none' viewBox='0 0 24 24' stroke='currentColor'>
-            <path strokeLinecap='round' strokeLinejoin='round' strokeWidth={2} d='M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z' />
-          </svg>
-        </button>
+      <button class='bg-gray-200 hover:bg-gray-400 text-black font-bold py-2 px-4 rounded-full'>
+        <svg xmlns='http://www.w3.org/2000/svg' className='h-6 w-6' fill='none' viewBox='0 0 24 24' stroke='currentColor'>
+          <path strokeLinecap='round' strokeLinejoin='round' strokeWidth={2} d='M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z' />
+        </svg>
+      </button>
           { likes > 0 ? (
             <p class='text-center'>{likes} likes</p>
           ) : (
