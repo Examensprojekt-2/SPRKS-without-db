23a56da11527c25b528926911bfb4951b59b8499
marcus <asker.marcus@gmail.com>
2021-11-25 10:36:05 +0100

    some wierd changes

---
+++ sprks/components/models/controllers/studentController.js
@@ -0,0 +1,31 @@
+const express = require('express')
+var router = express.Router()
+const mongoose = require('mongoose')
+const Student = mongoose.model('Student')
+
+router.get('/database', (req, res) => {
+    res.render('student/addOrEdit', {
+        viewTitle: 'Insert Student'
+    })
+})
+
+router.post('/database', (req, res) => {
+    if (req.body._id == '') {
+        insertRecord(req, res)
+    } else {
+        updateRecord(req, res)
+    }
+})
+
+function insertRecord(req, res) {
+    var student = new Student()
+    student.fullName = req.body.fullName
+    student.email = req.body.email
+    student.mobile = req.body.mobile
+    student.city = req.body.city
+    student.save((err, doc) => {
+        if (!err) {
+            res.redirect('student/list')
+        }
+    })
+}
